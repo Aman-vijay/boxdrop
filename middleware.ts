1b945da0a4e62aee4e3e67bbc6824befc0327669
Aman-vijay <shivamvj04@gmail.com>
2025-06-30 21:21:09 +0530

    Refactor middleware for route protection and update Tailwind config to use ES module syntax

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,32 @@
-import { clerkMiddleware , createRouteMatcher  } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server';
+import {
+  clerkMiddleware,
+  createRouteMatcher,
+  auth,
+} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoutes = createRouteMatcher(["/","sign-in(.*)?","sign-up(.*)?"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
+export default clerkMiddleware(async (auth, request) => {
+  const user = auth();
+  const userId = (await user).userId;
+  const url = new URL(request.url);
 
-
-export default clerkMiddleware(async(auth,request)=>{
-    const user = auth();
-    const userId = (await user)?.userId;
-    const url = new URL(request.url);
-
-    if(userId && isProtectedRoutes(request ) && url.pathname !== "/"){
-        return NextResponse.redirect(new URL("/dashboard", request.url));
-    }
-     if (!isProtectedRoutes(request)) {
-    await auth.protect()
+  if (userId && isPublicRoute(request) && url.pathname !== "/") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-})
+  // Protect non-public routes
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
- 
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
- 
-    '/(api|trpc)(.*)',
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
   ],
-}
+};
